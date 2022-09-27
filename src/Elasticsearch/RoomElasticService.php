3238269ecfda4b090e49c8ac00b3c1198025cd90
bilal <bilal.isler@dinodream.com>
2022-09-27 19:22:06 +0300

    room search query for elasticsearch

--- src/Elasticsearch/RoomElasticService.php
+++ src/Elasticsearch/RoomElasticService.php
@@ -21,7 +21,15 @@ class RoomElasticService extends AbstractElasticService
 
     public function searchRoomRequest(SearchRoomRequest $request)
     {
-        $result = $this->search([]);
+        $startDate = $request->getStartDate()->format('Y-m-d');
+        $endDate = $request->getEndDate()->format('Y-m-d');
+        $guestCount = $request->getGuestCount();
+        $country = $request->getCountry();
+        $city = $request->getCity();
+
+        $query = json_decode('{"query":{"bool":{"must":[{"range":{"order_date":{"gte":"'.$startDate.'","lte":"'.$endDate.'"}}},{"term":{"guestCount":{"value":'.$guestCount.'}}},{"term":{"country":{"value":"'.$country.'"}}},{"term":{"city":{"value":"'.$city.'"}}}]}}}',true);
+
+        $result = $this->search($query);
 
         return $result['hits']['hits'];
     }
